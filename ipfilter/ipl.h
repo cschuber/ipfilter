93a6135f18cfe17e9d0cc482ec55541587247e60
darren_r <>
2007-09-29 11:44:56 +0000

    update for 4.1.27

--- ipfilter/ipl.h
+++ ipfilter/ipl.h
@@ -10,8 +10,8 @@
 #ifndef	__IPL_H__
 #define	__IPL_H__
 
-#define	IPL_VERSION	"IP Filter: v4.1.26"
+#define	IPL_VERSION	"IP Filter: v4.1.27"
 
-#define	IPFILTER_VERSION	4012600
+#define	IPFILTER_VERSION	4012700
 
 #endif
