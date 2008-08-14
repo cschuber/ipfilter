df3bdc27c6487eaa852ee1adf9182da18f63a0a2
darren_r <>
2008-08-14 09:21:25 +0000

    bump to 5.0.3

--- ipfilter/ipl.h
+++ ipfilter/ipl.h
@@ -10,8 +10,8 @@
 #ifndef	__IPL_H__
 #define	__IPL_H__
 
-#define	IPL_VERSION	"IP Filter: v5.0.2"
+#define	IPL_VERSION	"IP Filter: v5.0.3"
 
-#define	IPFILTER_VERSION	5000002
+#define	IPFILTER_VERSION	5000003
 
 #endif
