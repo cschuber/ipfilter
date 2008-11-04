56154c63e9e9258fb39a73abc1ef34c289bf7742
darren_r <>
2008-11-04 21:39:10 +0000

    wakeup is not the macro WAKEUP everywhere

--- ipfilter/ip_sync.c
+++ ipfilter/ip_sync.c
@@ -912,7 +912,7 @@ void *ptr;
 # else
 	MUTEX_EXIT(&ipsl_mutex);
 #  ifdef _KERNEL
-	wakeup(&sl_tail);
+	WAKEUP(&sl_tail);
 	POLLWAKEUP(IPL_LOGSYNC);
 #  endif
 # endif
@@ -1002,7 +1002,7 @@ synclist_t *sl;
 # else
 	MUTEX_EXIT(&ipsl_mutex);
 #  ifdef _KERNEL
-	wakeup(&sl_tail);
+	WAKEUP(&sl_tail);
 	POLLWAKEUP(IPL_LOGSYNC);
 #  endif
 # endif
