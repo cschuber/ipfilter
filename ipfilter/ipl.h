20c1ea34e7977a99e53f683c89fc071173eb824d
darren_r <>
2009-01-01 07:26:26 +0000

    do 4.1.32rc1

--- ipfilter/ipl.h
+++ ipfilter/ipl.h
@@ -10,8 +10,8 @@
 #ifndef	__IPL_H__
 #define	__IPL_H__
 
-#define	IPL_VERSION	"IP Filter: v4.1.31"
+#define	IPL_VERSION	"IP Filter: v4.1.32_RC1"
 
-#define	IPFILTER_VERSION	4013100
+#define	IPFILTER_VERSION	4013201
 
 #endif
