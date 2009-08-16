6b70cb9c544b350b997e5ec2a1166213cdf6a719
darren_r <>
2009-08-16 07:07:43 +0000

    bump for 4.1.33

--- ipfilter/ipl.h
+++ ipfilter/ipl.h
@@ -10,8 +10,8 @@
 #ifndef	__IPL_H__
 #define	__IPL_H__
 
-#define	IPL_VERSION	"IP Filter: v4.1.32"
+#define	IPL_VERSION	"IP Filter: v4.1.33"
 
-#define	IPFILTER_VERSION	4013206
+#define	IPFILTER_VERSION	4013300
 
 #endif
