493642ece52d3d23d9598575c6ac01da183ea605
darren_r <>
2011-10-14 16:56:10 +0000

    3423808 ip addresses reportied incorrectly with ipfstat -f

--- ipfilter/lib/printfraginfo.c
+++ ipfilter/lib/printfraginfo.c
@@ -15,16 +15,26 @@ printfraginfo(prefix, ifr)
 	struct ipfr *ifr;
 {
 	frentry_t fr;
+	int family;
 
+	PRINTF("%s", prefix);
+	if (ifr->ipfr_v == 6) {
+		PRINTF("inet6");
+		family = AF_INET6;
+	} else {
+		PRINTF("inet");
+		family = AF_INET;
+	}
 	fr.fr_flags = 0xffffffff;
 
-	PRINTF("%s%s -> ", prefix, hostname(AF_INET, &ifr->ipfr_src));
+	PRINTF(" %s -> ", hostname(family, &ifr->ipfr_src));
 /*
 	if (kmemcpy((char *)&fr, (u_long)ifr->ipfr_rule,
 		    sizeof(fr)) == -1)
 		return;
  */
-	PRINTF("%s id %u ttl %lu pr %d seen0 %d ref %d\n",
-		hostname(AF_INET, &ifr->ipfr_dst), ifr->ipfr_id,
-		ifr->ipfr_ttl, ifr->ipfr_p, ifr->ipfr_seen0, ifr->ipfr_ref);
+	PRINTF("%s id %x ttl %lu pr %d pkts %u bytes %u seen0 %d ref %d\n",
+		hostname(family, &ifr->ipfr_dst), ifr->ipfr_id,
+		ifr->ipfr_ttl, ifr->ipfr_p, ifr->ipfr_pkts, ifr->ipfr_bytes,
+		ifr->ipfr_seen0, ifr->ipfr_ref);
 }
