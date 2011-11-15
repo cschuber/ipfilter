ab5786e1c01feb5020e1057ce53db6fcf442b67f
darren_r <>
2011-11-15 12:43:57 +0000

    3437749 ipf_fini_all called twice

--- ipfilter/solaris.c
+++ ipfilter/solaris.c
@@ -763,7 +763,6 @@ ipf_stack_init()
 static void
 ipf_stack_fini()
 {
-	ipf_fini_all(&ipfmain);
 	ipf_destroy_all(&ipfmain);
 }
 #else
@@ -998,8 +997,6 @@ static void
 ipf_instance_destroy(netid_t id, void *arg)
 {
 	ipf_main_softc_t *softc = arg;
-
-	ipf_fini_all(softc);
 
 	ipf_destroy_all(softc);
 }
