7b7f46949995549a38ce0e0d08946385f09a84cc
darren_r <>
2012-01-30 15:08:51 +0000

    v5.1.1

--- ipfilter/ipl.h
+++ ipfilter/ipl.h
@@ -10,8 +10,8 @@
 #ifndef	__IPL_H__
 #define	__IPL_H__
 
-#define	IPL_VERSION	"IP Filter: v5.1.0"
+#define	IPL_VERSION	"IP Filter: v5.1.1"
 
-#define	IPFILTER_VERSION	5010000
+#define	IPFILTER_VERSION	5010100
 
 #endif
