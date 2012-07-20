51f81c3fdb3276285ece06e6a208bb2779d6bece
cvs2git <>
2012-07-20 09:06:44 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_0'.

--- dns-proxy/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-YYDEBUG=1
-export YYDEBUG
-./dns-proxy -dddddddddd -f ./dns-proxy.conf
--- ipf-test-suite/new-ipf-test/bin/forwarding.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -f /usr/sbin/ndd ] ; then
-	ndd -set /dev/ip ip_forwarding 1
-fi
-if [ -f /sbin/sysctl ] ; then
-	sysctl -w net.inet.ip.forwarding=1
-fi
