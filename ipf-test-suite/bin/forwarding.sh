8bdae7e7f6891c060f138387a2060a31bcfe4c2d
darren_r <>
2014-06-07 14:36:27 +0000

    #540 absolute paths may be required for ndd/sysctl

--- ipf-test-suite/bin/forwarding.sh
+++ ipf-test-suite/bin/forwarding.sh
@@ -1,10 +1,10 @@
 #!/bin/ksh
 
 if [[ -f /usr/sbin/ndd ]] ; then
-	ndd -set /dev/ip ip_forwarding 1 2>&1
-	ndd -set /dev/ip ip6_forwarding 1 2>&1
+	/usr/sbin/ndd -set /dev/ip ip_forwarding 1 2>&1
+	/usr/sbin/ndd -set /dev/ip ip6_forwarding 1 2>&1
 fi
 if [[ -f /sbin/sysctl ]] ; then
-	sysctl -w net.inet.ip.forwarding=1 2>&1
-	sysctl -w net.inet6.ip6.forwarding=1 2>&1
+	/sbin/sysctl -w net.inet.ip.forwarding=1 2>&1
+	/sbin/sysctl -w net.inet6.ip6.forwarding=1 2>&1
 fi
